684f45fd425dce0d32634d0c14a820ab9eee34b4
oscarureta7-tech <oscar.ureta.7@gmail.com>
2025-10-23 21:41:21 -0700

    Project now references MicroLabEngine

--- space_shooter/scripts/SpaceShooter.cs
+++ space_shooter/scripts/SpaceShooter.cs
@@ -3,13 +3,14 @@ using System;
 
 public partial class SpaceShooter : Node
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// GODOT write debug message
+		GD.Print("SpaceShooter is ready!");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		GD.Print($"Frame updated. Delta time: {delta}");
 	}
 }
